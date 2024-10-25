721fa6cb3b1241f594c89fc110075d6b9b37f16b
akh44 <32739565+akh44@users.noreply.github.com>
2024-10-25 13:53:57 -0600

    Time: 2 ms (79.70%) | Memory: 44.3 MB (58.58%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+        int n=9;
+        HashSet<Character>[] rows=new HashSet[n];
+        HashSet<Character>[] cols=new HashSet[n];
+        HashSet<Character>[] boxes=new HashSet[n];
+        for(int r=0;r<n;r++)
+        {
+            rows[r]=new HashSet<Character>();
+            cols[r]=new HashSet<Character>();
+            boxes[r]=new HashSet<Character>();
+        }
+
+        for(int r=0;r<n;r++)
+        {
+            for(int c=0;c<n;c++)
+            {
+                char val=board[r][c];
+                if(val=='.')
+                {
+                    continue;
+                }
+                if(rows[r].contains(val))
+                {
+                    return false;
+                }
+                rows[r].add(val);
+                if(cols[c].contains(val))
+                {
+                    return false;
+                }
+                cols[c].add(val);
+
+                int idx=(r/3)*3+c/3;
+                if(boxes[idx].contains(val))
+                {
+                    return false;
+                }
+                boxes[idx].add(val);
+            }
+        }
+        return true;
+    }
+}
