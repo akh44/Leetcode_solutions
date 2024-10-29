02a510f9ce3796a4408ba2745d462bb73706e486
akh44 <32739565+akh44@users.noreply.github.com>
2024-10-29 16:13:56 -0600

    Time: 2 ms (98.63%) | Memory: 41.8 MB (50.87%) - LeetSync

--- 1083-two-sum-less-than-k/two-sum-less-than-k.java
+++ 1083-two-sum-less-than-k/two-sum-less-than-k.java
@@ -1,19 +1,19 @@
 class Solution {
     public int twoSumLessThanK(int[] nums, int k) {
-       Arrays.sort(nums);  // Sort the array to apply two-pointer technique
+       Arrays.sort(nums);  
         int i = 0;
         int j = nums.length - 1;
-        int max_sum = -1;  // Start with -1 to indicate no valid sum found if none exists
+        int max_sum = -1;  
 
         while (i < j) {
             int sum = nums[i] + nums[j];
 
             if (sum < k) {
-                // Update max_sum if the current sum is the highest valid sum found
+                
                 max_sum = Math.max(max_sum, sum);
-                i++;  // Try to increase sum by moving the left pointer
+                i++;  
             } else {
-                j--;  // Try to decrease sum by moving the right pointer
+                j--;  
             }
         }
 
