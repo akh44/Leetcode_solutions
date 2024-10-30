ec02b566439665a3dd9c06714d39165dcd1cd0bb
akh44 <32739565+akh44@users.noreply.github.com>
2024-10-30 16:44:47 -0600

    Time: 24 ms (97.63%) | Memory: 51.3 MB (64.36%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,53 +1,27 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        
-        if (nums == null || nums.length < 3) {
-            return result;
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; ++i) if (
+            i == 0 || nums[i - 1] != nums[i]
+        ) {
+            twoSumII(nums, i, res);
         }
-        
-        Arrays.sort(nums); // Sort the array to handle duplicates and use the two-pointer technique
-        
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue; // Skip duplicates for the first element
-            }
-            
-            int left = i + 1;
-            int right = nums.length - 1;
-            int target = -nums[i];
-            
-            while (left < right) {
-                int sum = nums[left] + nums[right];
-                
-                if (sum == target) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    
-                    // Move the left pointer to the next different number
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-                    
-                    // Move the right pointer to the previous different number
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
-                    
-                    left++;
-                    right--;
-                } else if (sum < target) {
-                    left++;
-                } else {
-                    right--;
-                }
-            }
-        }
-        
-        return result;
+        return res;
     }
 
+    void twoSumII(int[] nums, int i, List<List<Integer>> res) {
+        int lo = i + 1, hi = nums.length - 1;
+        while (lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if (sum < 0) {
+                ++lo;
+            } else if (sum > 0) {
+                --hi;
+            } else {
+                res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
+                while (lo < hi && nums[lo] == nums[lo - 1]) ++lo;
+            }
+        }
+    }
 }
