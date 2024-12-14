608811e8b8dbec5cf4caa1c501287b4e2078a185
akh44 <32739565+akh44@users.noreply.github.com>
2024-12-14 16:43:04 -0700

    Time: 7 ms (71.21%) | Memory: 43.3 MB (33.99%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,16 +1,34 @@
 public class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        s1 = sort(s1);
-        for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            if (s1.equals(sort(s2.substring(i, i + s1.length()))))
-                return true;
+        if (s1.length() > s2.length()) {
+            return false;
         }
-        return false;
-    }
-    
-    public String sort(String s) {
-        char[] t = s.toCharArray();
-        Arrays.sort(t);
-        return new String(t);
+
+        // Frequency arrays for s1 and the current window in s2
+        int[] freqS1 = new int[26];
+        int[] freqWindow = new int[26];
+
+        // Populate the frequency array for s1
+        for (char c : s1.toCharArray()) {
+            freqS1[c - 'a']++;
+        }
+
+        // Sliding window over s2
+        for (int i = 0; i < s2.length(); i++) {
+            // Add the current character to the window
+            freqWindow[s2.charAt(i) - 'a']++;
+
+            // Remove the leftmost character if the window size exceeds s1's length
+            if (i >= s1.length()) {
+                freqWindow[s2.charAt(i - s1.length()) - 'a']--;
+            }
+
+            // Compare the frequency arrays
+            if (Arrays.equals(freqS1, freqWindow)) {
+                return true; // Found a permutation of s1 in s2
+            }
+        }
+
+        return false; // No permutation found
     }
 }
