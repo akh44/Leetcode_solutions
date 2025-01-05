e68838646f97f159047607832edb6b054a7fc0f6
akh44 <32739565+akh44@users.noreply.github.com>
2025-01-04 17:08:11 -0700

    Time: 32 ms (47.99%) | Memory: 51.1 MB (67.23%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,50 +1,24 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
+        // Max-Heap to store points by distance
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> 
+            Integer.compare((b[0] * b[0] + b[1] * b[1]), (a[0] * a[0] + a[1] * a[1]))
+        );
 
-      ArrayList<axis> list=new ArrayList<>();
-      for(int i=0;i<points.length;i++)
-      {
-         axis obj = new axis(points[i][0],points[i][1]);
-         list.add(obj);
-      }
-      Comparator<axis> cmp= new Comparator<axis>()
-      {
-            public int compare(axis a1, axis a2)
-            {
-                int d1= (a1.x * a1.x) + (a1.y * a1.y);
-                int d2= (a2.x * a2.x) + (a2.y * a2.y);
-                if(d1>d2)
-                
-                    return 1;
-                
-                else
-                
-                    return -1;
-                
+        // Add points to the heap
+        for (int[] point : points) {
+            maxHeap.offer(point); // Add point to the heap
+            if (maxHeap.size() > k) {
+                maxHeap.poll(); // Remove the farthest point if heap exceeds size k
             }
-      };
+        }
 
-      Collections.sort(list,cmp);
-     
-      int ans[][]=new int[k][2];
-      for(int i=0;i<k;i++)
-      {
-        ans[i][0]=list.get(i).x;
-        ans[i][1]=list.get(i).y;
-      }
+        // Extract k closest points from the heap
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = maxHeap.poll();
+        }
 
-      return ans;
+        return result;
     }
 }
-      
-      class axis
-      {
-        int x;
-        int y;
-           public axis(int x,int y)
-           {
-            this.x=x;
-            this.y=y;
-           }
-      }
-
