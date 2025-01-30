1f0cdb3fac05416f23f18d13ee3eb1ac4ed06754
akh44 <32739565+akh44@users.noreply.github.com>
2025-01-29 18:29:50 -0700

    Time: 0 ms (100.00%) | Memory: 41 MB (79.29%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1) return nums[0];
+        if(n==2) return Math.max(nums[0],nums[1]);
+
+        return Math.max(robHelper(nums,0,n-2),robHelper(nums,1,n-1));
+        
+    }
+
+    public static int robHelper(int[] nums,int start,int end)
+    {
+        int prev1=0;
+        int prev2=0;
+
+        for(int i=start;i<=end;i++)
+        {
+            int current=Math.max(prev1,nums[i]+prev2);
+            prev2=prev1;
+            prev1=current;
+        }
+        return prev1;
+    }
+}
